a55aee818112f564ac7a2d1259d317fe762dfc74
Giancarlo Zuliani <slaigox@gmail.com>
2021-02-09 18:24:28 +0100

    vecchio progetto piallato

--- main/database/seeds/DatabaseSeeder.php
+++ main/database/seeds/DatabaseSeeder.php
@@ -11,10 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([
-            EmployeeSeeder::class,
-            TaskSeeder::class,
-            TypologySeeder::class,
-        ]);
+        // $this->call(UsersTableSeeder::class);
     }
 }
--- main/routes/web.php
+++ main/routes/web.php
@@ -1,54 +1,18 @@
 <?php
 
-
 use Illuminate\Support\Facades\Route;
 
-Route::get("/", "MainController@index")
-     -> name("employees-index");
-Route::get("/employee/{id}", "MainController@show")
-     -> name("employee-show");
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
-Route::get("/create/employee", "MainController@create")
-     -> name("employee-create");
-Route::post("/store/employee", "MainController@store")
-     -> name("employee-store");
-
-Route::get("/edit/employee/{id}", "MainController@edit") 
-    -> name("employee-edit");
-Route::post("/update/employee/{id}", "MainController@update")
-     -> name("employee-update");
-
-
-
-Route::get("/tasks", "TaskController@index")
-     -> name("tasks-index");
-Route::get("/task/{id}", "TaskController@show")
-     -> name("task-show");
-
-Route::get("/create/task", "TaskController@create")
-     -> name("task-create");
-Route::post("/store/task", "TaskController@store")
-     -> name("task-store");
-
-Route::get("/edit/task/{id}", "TaskController@edit")
-     -> name("task-edit");
-Route::post("/update/task/{id}", "TaskController@update")
-     -> name("task-update");
-
-
-
-
-Route::get("/typologies", "TypologyController@index")
-     -> name("typologies-index");
-Route::get("/typology/{id}", "TypologyController@show")
-     -> name("typology-show");
-
-Route::get("/create/typology", "TypologyController@create")
-     -> name("typology-create");
-Route::post("/store/typology", "TypologyController@store")
-     -> name("typology-store");
-
-Route::get("/edit/typology/{id}", "TypologyController@edit")
-     -> name("typology-edit");
-Route::post("/update/typology/{id}", "TypologyController@update")
-     -> name("typology-update");
+Route::get('/', function () {
+    return view('welcome');
+});
